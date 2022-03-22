6646382b7584bfc0e533fe11e98e686308d737a1
Terry Brooks, Jr <terry.arthur@brooksjr.com>
2022-03-21 19:08:27 -0500

    db completed and connected - Started Security

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = MongoEngine()
-db.init_app(app)
+db_NoSQL = MongoEngine()
+db_NoSQL.init_app(app)
+db_SQL = SQLAlchemy(app)
+
 
 from application import routes
 
