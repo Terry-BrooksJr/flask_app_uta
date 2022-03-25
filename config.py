14de4db09c247d31f0d4613b619a71ab6eb5f04c
Terry Brooks, Jr <terry.arthur@brooksjr.com>
2022-03-24 20:28:13 -0500

    add flash messaging

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('IoDDW1PoKh&$lQ0@^pj6msL#YTVVwAJFFoZpO*m8n1&7') 
+    SECRET_KEY = 'IoDDW1PoKh&$lQ0@^pj6msL#YTVVwAJFFoZpO*m8n1&7'
     MONGODB_SETTINGS = { 'db':'uta_enrollment'}
     SQLALCHEMY_DATABASE_URI = 'mysql://Terry:I0t@C0d3sling3r@localhost/uta'
     SQLALCHEMY_ECHO = True  
