9752262b74a2234e617f9a7a4b70b5e11cd61de2
Terry Brooks, Jr <terry.arthur@brooksjr.com>
2022-03-27 19:12:58 -0500

    finish adding flash messaging

--- application/forms.py
+++ application/forms.py
@@ -1,18 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired 
+from wtforms.validators import DataRequired
+
 
 class LoginForm(FlaskForm):
     email = StringField('E-Mail/Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()])
-    
+
     remember_me = BooleanField('Remember Me?')
     submit = SubmitField('Login')
-    
+
+
 class RegistrationForm(FlaskForm):
     email = StringField('E-Mail/Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()])
-    confirm_password = StringField('Confirm Password',validators=[DataRequired()])
-    first_name = StringField('First Name',validators=[DataRequired()])
+    confirm_password = StringField(
+        'Confirm Password', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     register = SubmitField('Sign Up')
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db_NoSQL
-from flask import render_template, request, json, Response, flash, redirect
+from flask import render_template, request, json, Response, flash, redirect, url_for
 from application.models import User, Courses, Enrollment
 from application.forms import LoginForm, RegistrationForm
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
@@ -17,11 +17,12 @@ def index():
 def login():
     form = LoginForm
     if form.validate_on_submit:
-        if request.form.email == 'terry@brooksjr.com':
-        flash("You have successfully logged in")
-        return redirect(url_for(TBD)))
-    else:
-        return render_template("login.html", login=True,title='Login',form=form() )
+        if request.form.get('email') == 'terry@brooksjr.com':
+            flash("You have successfully logged in!", "success")
+            return redirect("/index")
+        else:
+            flash('Email and Password Combination Not Found', "danger")
+    return render_template("login.html", login=True,title='Login',form=form() )
 
 @app.route("/courses/")
 @app.route("/courses/<term>")
